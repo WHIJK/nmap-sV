b5148cec58f663c5d2ce1a944abc753fd846e04f
OvO <None>
2023-11-02 00:12:56 +0800

    add finger

--- nmap-sV.go
+++ nmap-sV.go
@@ -86,6 +86,9 @@ func init() {
 	core.AddPattern(&nmapStructs, "TerminalServerCookie", "^\\x03\\x00\\x00\\x13\\x0e\\xd0\\x00\\x00\\x124\\x00\\x02.*\\x02\\x00\\x00\\x00",
 		"ms-wbt-server", "", "o:microsoft:windows", "", "", "", "Windows", "Microsoft Terminal Services",
 		"Windows 7 or Server 2008 R2")
+	core.AddPattern(&nmapStructs, "GetRequest", "^HTTP/1\\.[1\\|0]",
+		"http", "", "", "", "", "", "", "",
+		"")
 }
 
 func main() {
